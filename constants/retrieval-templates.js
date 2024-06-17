9aa9f61a4645e27d8b1287bffd6dc28025d7f68d
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2024-06-17 14:22:20 +0300

    updates retrieval template

--- constants/retrieval-templates.js
+++ constants/retrieval-templates.js
@@ -1,4 +1,4 @@
-const CONDENSE_QUESTION_TEMPLATE = `Având în vedere următoarea conversație, detaliile despre animalul de companie și o întrebare ulterioară, reformulează întrebarea ulterioară pentru a fi o întrebare independentă în română.
+const CONDENSE_QUESTION_TEMPLATE = `Având în vedere următoarea conversație, detaliile despre animalul de companie și o întrebare ulterioară, reformulează întrebarea ulterioară pentru a fi o întrebare independentă în română. Întrebarea va face o cerere de recomandare de produs de hrana, de igiena, suplimente medicale, jucarii sau produse de igiena.
 
 <chat_history>
   {chat_history}
@@ -31,10 +31,22 @@ Instrucțiuni suplimentare:
 - Recomandă produse pentru juniori dacă vârsta animalului este sub 12 luni.
 - Nu recomanda produse care conțin cuvântul "adult" în titlu dacă vârsta este sub 12 luni.
 - Menționează doar produsele relevante pentru specia, rasa, vârsta, sexul și problemele de sănătate menționate în detaliile despre animalul de companie.
+- Cand vine vorba de hrana, e doar preferabil sa se potriveasca la fix pentru rasa animalului, dar nu obligatoriu. La fel si in functie de varsta.
 - Nu face recomandări dacă nu ai suficiente informații.
-- Răspunde întotdeauna în română.
 
 Întrebare: {question}
 `;
 
 export { CONDENSE_QUESTION_TEMPLATE, ANSWER_TEMPLATE };
+
+/**
+ * Instrucțiuni suplimentare:
+
+- Nu recomanda produse pentru juniori dacă vârsta animalului este peste 12 luni.
+- Recomandă produse pentru juniori dacă vârsta animalului este sub 12 luni.
+- Nu recomanda produse care conțin cuvântul "adult" în titlu dacă vârsta este sub 12 luni.
+- Menționează doar produsele relevante pentru specia, rasa, vârsta, sexul și problemele de sănătate menționate în detaliile despre animalul de companie.
+- Nu face recomandări dacă nu ai suficiente informații.
+- Răspunde întotdeauna în română.
+
+ */
